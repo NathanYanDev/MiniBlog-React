ff02fe6af45701da796c38c74781e65ee6798633
Nathan Yan <nathan_yan_alves@hotmail.com>
2024-12-21 15:55:28 -0300

    Setup firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
+import {app} from "../firebase/config"
 
 // To add components `bunx --bun shadcn@latest add`
 
 function App() {
+	console.log(app)
 	return (
 		<div className='container'>
 			<h1 className='text-2xl'>Mini Blog</h1>
